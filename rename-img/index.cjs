b82f773dcc5565a18ccdaa88ecb438885093f1a0
qzda <qinzhidaoa@gmail.com>
2024-03-27 23:20:05 +0800

    feat: 支持按拍摄时间重命名

--- rename-img/index.cjs
+++ rename-img/index.cjs
@@ -1,63 +1,47 @@
-const fs = require('fs');
-const ExifImage = require('exif').ExifImage;
+const fs = require("fs");
+const ExifImage = require("exif").ExifImage;
 
-const directory = './img';
-const outPutDirectory = './dist'
+const inputDirectory = "./input";
+const outputDirectory = "./output";
 
-fs.readdir(directory, (err, files) => {
+fs.readdir(inputDirectory, (err, files) => {
   if (err) {
-    console.error('Error reading directory:', err);
+    console.error(`读取文件夹错误：'${inputDirectory}'`);
     return;
   }
 
-  files.forEach(file => {
+  console.log(`共${files.length}个文件\n`);
+
+  files.forEach((file) => {
     // 匹配格式
     if (file.match(/\.(jpg|jpeg)$/i)) {
-      new ExifImage({ image: `${directory}/${file}` }, function (error, exifData) {
-        if (error)
-          console.log('Error: ' + error.message);
-        else
-          console.log(exifData.image); // Do something with your data!
+      const inputFilePath = `${inputDirectory}/${file}`;
+      new ExifImage({ image: inputFilePath }, function (error, exifData) {
+        if (error) {
+          console.error(`获取文件信息失败：${inputFilePath}` + error.message);
+        } else {
 
-        const { } = exifData.image
+          const { ModifyDate } = exifData.image;
+
+          // console.log(ModifyDate)
+          // return
+
+          const outputFileName = `${ModifyDate.split(':').join('-')}.${file.split('.').at(-1)}`;
+          const outputFilePath = `${outputDirectory}/${outputFileName}`;
+
+          fs.rename(inputFilePath, outputFilePath, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`重命名文件："${inputFilePath} -> ${outputFilePath}" 成功！`);
+            }
+          });
+        }
       });
-
-      // fs.readFile(`${directory}/${file}`, (err, data) => {
-      //   if (err) {
-      //     console.error('Error reading file:', err);
-      //     return;
-      //   }
-
-      //   exif.read(data, (err, result) => {
-      //     if (err) {
-      //       console.error('Error reading EXIF data:', err);
-      //       return;
-      //     }
-      //     console.log('result', result)
-      //     return
-      //     if (result && result.DateTimeOriginal) {
-      //       const date = new Date(result.DateTimeOriginal);
-      //       const year = date.getFullYear();
-      //       const month = ('0' + (date.getMonth() + 1)).slice(-2);
-      //       const day = ('0' + date.getDate()).slice(-2);
-      //       const hours = ('0' + date.getHours()).slice(-2);
-      //       const minutes = ('0' + date.getMinutes()).slice(-2);
-      //       const seconds = ('0' + date.getSeconds()).slice(-2);
-      //       const newName = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}${file.match(/\.(jpg|jpeg)$/i)[0]}`;
-      //       const newFilePath = `${outPutDirectory}/${newName}`;
-
-      //       fs.rename(`${directory}/${file}`, newFilePath, (err) => {
-      //         if (err) {
-      //           console.error(`Error renaming file ${file} to ${newName}:`, err);
-      //         } else {
-      //           console.log(`Renamed ${file} to ${newName}`);
-      //         }
-      //       });
-      //     } else {
-      //       console.log(`No DateTimeOriginal found for file ${file}`);
-      //     }
-      //   });
-      // });
+    } else {
+      console.error('不可转换的文件格式：', file)
     }
+
+    console.log('\n')
   });
 });
