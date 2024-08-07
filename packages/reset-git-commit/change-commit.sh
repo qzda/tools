cbae52d3e3dbe3fb033eff404d1644290ae7d36d
qzda <qinzhidaoa@gmail.com>
2024-08-07 17:15:28 +0800

    feat(reset-git-commit): add '-f'

--- packages/reset-git-commit/change-commit.sh
+++ packages/reset-git-commit/change-commit.sh
@@ -17,6 +17,6 @@ then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-' --tag-name-filter cat -- --branches --tags
+' -f --tag-name-filter cat -- --branches --tags
 
 git push --force --tags origin 'refs/heads/*'
